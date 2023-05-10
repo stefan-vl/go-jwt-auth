ca68bbf2f9dda4169cef108ac8a52aebb804c700
StefanS <vlad.sinescu@ezugi.com>
2023-05-10 16:13:15 +0300

    jwt token auth with using go, mongo & gin

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	routes "github.com/stefan-vl/go-jwt-auth/routes"
+	"log"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(context *gin.Context) {
+		context.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(context *gin.Context) {
+		context.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	err := router.Run(port)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
